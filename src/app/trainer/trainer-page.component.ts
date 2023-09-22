00ebfa527be681a607f58b89555ac5f5e61ed425
Fredrik Holm Julsen <Fredrjul@stud.ntnu.no>
2023-09-22 15:33:59 +0200

    trainerPage init

--- src/app/trainer/trainer-page.component.ts
+++ src/app/trainer/trainer-page.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserService} from '../services/user.service';
+import { User } from '../models/user.models';
 
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer-page.component.html',
 })
 export class TrainerPageComponent {
+  @Input() user: User | undefined;
+  constructor(public userService: UserService) { }
 
+  ngOnInit() {
+    this.user = this.userService.getUser();
+  }
 }
