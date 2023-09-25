168c43d075c8339f809b8d49ecdbc1b528c11cea
Haukaun <haakonfs@hotmail.com>
2023-09-25 08:58:36 +0200

    Fix: authgard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.userService.getUser() || localStorage.getItem('user')) return true;
+    this.router.navigate(['']);
+    return false;
+  }
+
+  canActivateTeam: CanActivateFn =
+  (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    return inject(AuthGuardService).canActivate(inject(this.userService.getUser), route.params['id']);
+  };
+
 }
